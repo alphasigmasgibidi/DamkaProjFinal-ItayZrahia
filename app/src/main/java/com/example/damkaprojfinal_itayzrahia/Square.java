1c39bf5a97178b0a1ddc4f9bf516a404b1caeb38
alphasigmasgibidi <itayzsr@gmail.com>
2025-12-24 12:04:18 +0200

    s

--- app/src/main/java/com/example/damkaprojfinal_itayzrahia/Square.java
+++ app/src/main/java/com/example/damkaprojfinal_itayzrahia/Square.java
@@ -21,13 +21,4 @@ public class Square extends Shape{
 
         canvas.drawRect(x,y,x+w,y+h,p);
     }
-
-    public boolean didXandYInSquare(float xc, float yc)
-    {
-        // xc & yc are the coin location
-        // check if the point in the middle of the circle, is in the square
-        if(xc > x && xc < x+w && yc > y && yc < y+h)
-            return  true;
-        return false;
-    }
 }
