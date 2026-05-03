baf4e878f7cde2ecc6eb000731700c6b8b950282
alphasigmasgibidi <itayzsr@gmail.com>
2026-05-03 10:51:01 +0300

    fdfddfg

--- app/src/main/java/com/example/damkaprojfinal_itayzrahia/GameModule.java
+++ app/src/main/java/com/example/damkaprojfinal_itayzrahia/GameModule.java
@@ -34,72 +34,61 @@ public class GameModule
         }
     }
 
-    public int checkMove(Coin c, int row_NEW, int col_NEW, ArrayList<Coin> all)
+    public int checkMove(Coin c, int rowNew, int colNew, ArrayList<Coin> all)
     {
 
-        King k = new King(all.remove(14));
-        all.add(5, k);
-        King kimk = new King(all.remove(2));
-        all.add(2, kimk);
-
-        if (col_NEW < 0 || row_NEW > 7 || row_NEW < 0 || col_NEW > 7) //-*Checks if coin is placed inside the board*-
+        if (colNew < 0 || rowNew > 7 || rowNew < 0 || colNew > 7) //-*Checks if coin is placed inside the board*-
         {
             return MOVE_ILEGAL;
         }
 
-        if ((row_NEW + col_NEW) % 2 == 0) //-*Checks if coin is placed on Dark squares*-
+        if ((rowNew + colNew) % 2 == 0) //-*Checks if coin is placed on Dark squares*-
         {
             return MOVE_ILEGAL;
         }
 
         for (int i = 0; i < all.size(); i++) //-*Checks if coin is placed on empty squares*- 
         {
-            if (all.get(i).row == row_NEW)
+            if (all.get(i).row == rowNew)
             {
-                if (all.get(i).col == col_NEW)
+                if (all.get(i).col == colNew)
                 {
                     return MOVE_ILEGAL;
                 }
             }
         }
 
-        int row_CURRENT = c.row;
+        int rowCurrent = c.row;
         
-        int col_CURRENT = c.col;
+        int colCurrent = c.col;
         
         
-        int row_HEFRESH = row_NEW - row_CURRENT;
+        int rowHefresh = rowNew - rowCurrent;
         
-        int col_HEFRESH = col_NEW - col_CURRENT;
+        int colHefresh = colNew - colCurrent;
 
 
-        int row_HEFRESH_ABSOLUTE = row_HEFRESH;
+        int rowHefreshAbsolute = rowHefresh;
 
 
-        if (row_HEFRESH_ABSOLUTE < 0) // making the row_HEFRESH a positive number always
+        if (rowHefreshAbsolute < 0) // making the rowHefresh a positive number always
         {
-            row_HEFRESH_ABSOLUTE = -row_HEFRESH_ABSOLUTE;
+            rowHefreshAbsolute = -rowHefreshAbsolute;
         }
 
 
-        int col_HEFRESH_ABSOLUTE = col_HEFRESH;
+        int colHefreshAbsolute = colHefresh;
 
 
-        if (col_HEFRESH_ABSOLUTE < 0) // making the col_HEFRESH a positive number always
+        if (colHefreshAbsolute < 0) // making the colHefresh a positive number always
         {
-            col_HEFRESH_ABSOLUTE = -col_HEFRESH_ABSOLUTE;
+            colHefreshAbsolute = -colHefreshAbsolute;
         }
 
-
-        int index = all.indexOf(c); // For The sake of tidy code we split the moves
-        if (all.get(index).type == "king")
-        {
-            return checkMoveIfKing(c, row_NEW, col_NEW, all, row_HEFRESH_ABSOLUTE, col_HEFRESH_ABSOLUTE, row_CURRENT, col_CURRENT );
-        }
 
         if (c.team == Coin.TEAM_BANANA) // if there's a jump of more than one square for Banana
         {
-            if (row_HEFRESH >= 0)
+            if (rowHefresh >= 0)
             {
                 return MOVE_ILEGAL;
             }
@@ -107,26 +96,26 @@ public class GameModule
 
         if (c.team == Coin.TEAM_STRAWBERRY) // if there's a jump of more than one square for Strawberry
         {
-            if (row_HEFRESH <= 0)
+            if (rowHefresh <= 0)
             {
                 return MOVE_ILEGAL;
             }
         }
 
-        if (row_HEFRESH_ABSOLUTE == 1)
+        if (rowHefreshAbsolute == 1)
         {
-            if (col_HEFRESH_ABSOLUTE == 1)
+            if (colHefreshAbsolute == 1)
             {
                 return MOVE_REGULAR;
             }
         }
 
-        if (row_HEFRESH_ABSOLUTE == 2)
+        if (rowHefreshAbsolute == 2)
         {
-            if (col_HEFRESH_ABSOLUTE == 2)
+            if (colHefreshAbsolute == 2)
             {
-                int midR = (row_CURRENT + row_NEW) / 2;
-                int midC = (col_CURRENT + col_NEW) / 2;
+                int midR = (rowCurrent + rowNew) / 2;
+                int midC = (colCurrent + colNew) / 2;
 
                 for (int i = 0; i < all.size(); i++)
                 {
@@ -146,34 +135,6 @@ public class GameModule
 
         return MOVE_ILEGAL;
     }
-
-    public int checkMoveIfKing(Coin c, int row_NEW, int col_NEW, ArrayList<Coin> all, int row_HEFRESH_ABSOLUTE, int col_HEFRESH_ABSOLUTE, int row_CURRENT, int col_CURRENT ) {
-
-        if (row_HEFRESH_ABSOLUTE == col_HEFRESH_ABSOLUTE)
-        {
-                int midR = (row_CURRENT + row_NEW) - 1;
-                int midC = (col_CURRENT + col_NEW) - 1;
-
-                for (int i = 0; i < all.size(); i++)
-                {
-                    if (all.get(i).row == midR)
-                    {
-                        if (all.get(i).col == midC)
-                        {
-                            if (all.get(i).team != c.team)
-                            {
-                                return MOVE_EAT;
-                            }
-                        }
-                    }
-                }
-            }
-
-
-        return MOVE_REGULAR;
-
-    }
-
     public int checkWinner(ArrayList<Coin> allCoins) {
         boolean doesWhiteExist = false;
         boolean doesRedExist = false;
--- app/src/main/java/com/example/damkaprojfinal_itayzrahia/GameView.java
+++ app/src/main/java/com/example/damkaprojfinal_itayzrahia/GameView.java
@@ -107,7 +107,7 @@ public class GameView extends View
                     float coin1st_X = c * tileSize + tileSize / 2;
                     float coin1st_Y = BOARD_STARTS_FROM + r * tileSize + tileSize / 2;
 
-                    if (r < 3) //top of board (Strawberry Team Pieces)
+                    if (r < 1) //top of board (Strawberry Team Pieces)
                     {
                         coins.add(new Coin(coin1st_X, coin1st_Y, radius, Color.parseColor("#ffc9d4"), Coin.TEAM_STRAWBERRY, r, c));
                     }
