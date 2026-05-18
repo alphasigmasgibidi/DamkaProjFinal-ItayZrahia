015c834d9e516ce46be25611623cd61e701af62f
alphasigmasgibidi <itayzsr@gmail.com>
2026-05-18 15:24:01 +0300

    newest

--- app/src/main/java/com/example/damkaprojfinal_itayzrahia/InstructionsActivity.java
+++ app/src/main/java/com/example/damkaprojfinal_itayzrahia/InstructionsActivity.java
@@ -1,20 +1,29 @@
 package com.example.damkaprojfinal_itayzrahia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InstructionsActivity extends AppCompatActivity
-{
+public class InstructionsActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnBack;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(this);
     }
 
-    public void onBackClick(View v)
-    {
-        finish();
+    @Override
+    public void onClick(View view) {
+        if (view == btnBack)
+        {
+            finish();
+        }
     }
 }
